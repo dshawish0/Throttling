b26ff543dfab0ff907aa4448eee65bdedff99a4c
DEIA'A ALSHAWISH <daalshawish20@cit.just.edu.jo>
2023-06-09 15:40:48 +0300

    create Profile Controller, Repository and Service and retrun url

---
+++ src/main/java/com/deiaa/throttling/controller/ProfileController.java
@@ -0,0 +1,20 @@
+package com.deiaa.throttling.controller;
+
+import com.deiaa.throttling.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+
+    @Autowired
+    private ProfileService profileService;
+
+    @GetMapping
+    public String getProfile() {
+        return profileService.getProfile();
+    }
+}
